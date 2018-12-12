8237320dd7f8d2560f52676c50d788e707c63b92
Seb Dalgarno <seb@poissonconsulting.ca>
2018-12-11 19:14:05 -0800

    update data raw script

--- data-raw/lookups.R
+++ data-raw/lookups.R
@@ -5,37 +5,30 @@ dsn_wsl <- "~/Poisson/Data/spatial/fwa/gdb/FWA_WATERSHED_BOUNDARIES_SP.gdb/"
 dsn_wsp <- "~/Poisson/Data/spatial/fwa/gdb/FWA_WATERSHEDS_POLY.gdb/"
 dsn_lb <- "~/Poisson/Data/spatial/fwa/gdb/FWA_LINEAR_BOUNDARIES_SP.gdb/"
 
-fwa_meta <- list(fwa_bc = st_layers(dsn_bc),
-                 fwa_watershed_line = st_layers(dsn_wsl),
-                 fwa_watershed_polygon = st_layers(dsn_wsp),
-                 fwa_linear_boundaries = st_layers(dsn_lb))
+fwa_metadata <- list(FWA_BC = st_layers(dsn_bc),
+                 FWA_WATERSHED_BOUNDARIES_SP = st_layers(dsn_wsl),
+                 FWA_WATERSHEDS_POLY = st_layers(dsn_wsp),
+                 FWA_LINEAR_BOUNDARIES_SP = st_layers(dsn_lb))
 
-fwa_layers <- lapply(fwa_meta, function(x) x$name[!is.na(x$geomtype)])
+fwa_layers <- lapply(fwa_metadata, function(x) x$name[!is.na(x$geomtype)])
+fwa_layers <- map_dfr(names(fwa_metadata), function(x){
+  data <- fwa_metadata[x]
+  tibble(db = x, layer = data[[1]]$name)
+})
 
-to_shortcut <- function(x){
-  ex <- grep("_max|_fwa|50K|CODES", x, value = T)
-  x <- setdiff(x, ex) %>%
-    tolower %>%
-    gsub("fwa_|_poly|_sp", "", .)
-  x
-}
-
-fwa_layers$fwa_bc <- tibble(layer = fwa_layers$fwa_bc, shortcut = to_shortcut(fwa_layers$fwa_bc))
-fwa_layers[2:4] <- lapply(fwa_layers[2:4], function(x) tibble(layer = x, shortcut = wscode_to_name(x)))
-
-usethis::use_data(fwa_meta, overwrite = TRUE)
+usethis::use_data(fwa_metadata, overwrite = TRUE)
 usethis::use_data(fwa_layers, overwrite = TRUE)
 
-blk <- st_read(dsn_bc, layer = "FWA_ROUTES_SP", query = "select BLUE_LINE_KEY, FWA_WATERSHED_CODE from FWA_ROUTES_SP")
+route <- st_read(dsn_bc, layer = "FWA_ROUTES_SP", query = "select BLUE_LINE_KEY, FWA_WATERSHED_CODE, WATERSHED_KEY from FWA_ROUTES_SP")
+
+fwa_stream_lookup <- route %>%
+  st_set_geometry(NULL) %>%
+  select(BlueLineKey = BLUE_LINE_KEY,
+         WatershedCode = FWA_WATERSHED_CODE,
+         WatershedKey = WATERSHED_KEY)
+
 ws <- st_read(dsn_bc, layer = "FWA_NAMED_WATERSHEDS_POLY") %>%
   select(GNIS_NAME, BLUE_LINE_KEY, STREAM_ORDER)
-wsgroup <- st_read(dsn_bc, layer = "FWA_WATERSHED_GROUPS_POLY")
-
-fwa_blk_stream <- blk %>%
-  st_set_geometry(NULL) %>%
-  select(BlueLineKey = BLUE_LINE_KEY)
-
-usethis::use_data(fwa_blk_stream, overwrite = TRUE)
 
 fwa_gnis <- ws %>%
   st_set_geometry(NULL) %>%
@@ -44,9 +37,16 @@ fwa_gnis <- ws %>%
 fwa_gnis$GnisName %<>% as.character
 
 # remove Colley Creek BlueLineKey that isn't present in the route table
-fwa_gnis %<>% filter(!(BlueLineKey %in% anti_join(gnis, blkey, "BlueLineKey")$BlueLineKey))
+fwa_gnis %<>% filter(!(BlueLineKey %in% anti_join(fwa_gnis, fwa_stream_lookup, "BlueLineKey")$BlueLineKey))
+check_join(fwa_gnis, fwa_stream_lookup, "BlueLineKey")
+fwa_stream_lookup <- left_join(fwa_stream_lookup, fwa_gnis, "BlueLineKey")
 
-usethis::use_data(fwa_gnis, overwrite = TRUE)
+usethis::use_data(fwa_stream_lookup, overwrite = TRUE)
+
+fwa_gnis_lookup <- fwa_gnis
+usethis::use_data(fwa_gnis_lookup, overwrite = TRUE)
+
+wsgroup <- st_read(dsn_bc, layer = "FWA_WATERSHED_GROUPS_POLY")
 
 fwa_watershed_group <- wsgroup %>%
   st_set_geometry(NULL) %>%
@@ -55,12 +55,15 @@ fwa_watershed_group <- wsgroup %>%
 fwa_watershed_group$WatershedGroupCode %<>% as.character
 fwa_watershed_group$WatershedGroupName %<>% as.character
 
-usethis::use_data(fwa_watershed_group, overwrite = TRUE)
+fwa_wsgroup_lookup <- fwa_watershed_group
+usethis::use_data(fwa_wsgroup_lookup, overwrite = TRUE)
 
-fwa_blk_coastline <- st_read(dsn_bc, layer = "FWA_COASTLINES_SP",
-                             query = "select BLUE_LINE_KEY from FWA_COASTLINES_SP")  %>%
+fwa_coastline_lookup <- st_read(dsn_bc, layer = "FWA_COASTLINES_SP",
+                             query = "select * from FWA_COASTLINES_SP") %>%
   st_set_geometry(NULL) %>%
-  select(BlueLineKey = BLUE_LINE_KEY)
+  select(BlueLineKey = BLUE_LINE_KEY,
+         WatershedCode = FWA_WATERSHED_CODE,
+         WatershedKey = WATERSHED_KEY)
 
-usethis::use_data(fwa_blk_coastline, overwrite = TRUE)
+usethis::use_data(fwa_coastline_lookup, overwrite = TRUE)
 
