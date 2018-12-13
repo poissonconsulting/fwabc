9d01104a261ae1fbf6035b4ea96d9f89e89b2f09
Seb Dalgarno <seb@poissonconsulting.ca>
2018-12-12 19:09:17 -0800

    add tests for translators and tribs

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,25 @@
 context("utils")
 
-test_that("utils", {
+test_that("utils work", {
 
+  ### translators
+  x <- stream_to_blk("Kaslo River")
+  expect_length(x, 1L)
+  expect_is(x, "integer")
+  expect_true(x == 356567321L)
+
+  x <- stream_to_wscode("Kaslo River")
+  expect_length(x, 1L)
+  expect_is(x, "character")
+  expect_true(x == 356567321L)
+
+  x <- wsgname_to_wsgcode("PORI")
+  expect_length(x, 1L)
+  expect_is(x, "character")
+  expect_true(x == "Porcher Island")
+
+  ### tributaries
+  x <- tribs(stream_to_wscode("Chown River"))
+  expect_is(x, "integer")
+  expect_length(x, 8L)
 })
