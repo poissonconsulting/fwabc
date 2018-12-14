9f4d7cfb922fca387f4320ae3a14dde076e7aece
Seb Dalgarno <seb@poissonconsulting.ca>
2018-12-13 22:34:21 -0800

    add regex search functions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,40 @@
+#' Search valid GnisNames.
+#'
+#' @param pattern A character string containing a regular expression (or character string for fixed = TRUE) to be matched.
+#' @param ignore_case A flag indicating whether to ignore case.
+#' @param ... Arguments passed to grepl.
+#' @return A character vector.
+#' @examples
+#' fwa_search_gnis("kaslo")
+#' @export
+fwa_search_gnis <- function(pattern, ignore_case = TRUE, ...){
+  fwa_lookup_stream_gnis$GnisName[grepl(pattern, fwa_lookup_stream_gnis$GnisName, ignore.case = ignore_case, ...)]
+}
+
+#' Search valid watershed groups.
+#'
+#' @param pattern A character string containing a regular expression (or character string for fixed = TRUE) to be matched.
+#' @param ignore_case A flag indicating whether to ignore case.
+#' @param coast A flag indicating whether to only search watershed groups in the coastline layer.
+#' @param code A flag indicating whether to search by watershed group code (not name).
+#' @param ... Arguments passed to grepl.
+#' @return A character vector.
+#' @examples
+#' fwa_search_wsgroup("graham")
+#' @export
+fwa_search_wsgroup <- function(pattern, ignore_case = TRUE, coast = FALSE, code = FALSE, ...){
+  x <- fwa_lookup_wsgroup
+  if(coast){
+    x <- fwa_lookup_coast_wsgroup
+  }
+  y <- x$WatershedGroupName
+  if(code){
+    y <- x$WatershedGroupCode
+  }
+  y[grepl(pattern, y, ignore.case = ignore_case, ...)]
+}
+
+
 stream_to_blk <- function(x){
   unlist(lapply(x, function(x){
     if(is_blk_stream(x)){
@@ -22,6 +59,15 @@ stream_to_wscode <- function(x){
   }))
 }
 
+wsgname_to_wsgcode <- function(x){
+  unlist(lapply(x, function(x){
+    if(!(is_wsg_name(x))){
+      return(x)
+    }
+    fwa_lookup_wsgroup$WatershedGroupCode[fwa_lookup_wsgroup$WatershedGroupName %in% x]
+  }))
+}
+
 tribs_stream <- function(x){
   x <- gsub("-000000", "", x)
   unlist(lapply(x, function(x){
@@ -36,15 +82,7 @@ tribs_wshed <- function(x){
   }))
 }
 
-wsgname_to_wsgcode <- function(x){
-  checkor(check_ws(x), is.null(x))
-  unlist(lapply(x, function(x){
-    if(is_wsg_code(x)){
-      return(x)
-    }
-    fwa_lookup_wsgroup$WatershedGroupCode[fwa_lookup_wsgroup$WatershedGroupName %in% x]
-  }))
-}
+
 
 line_sample <- function(data, distance){
   # data <- try(st_cast(data, "LINESTRING", silent = TRUE))
