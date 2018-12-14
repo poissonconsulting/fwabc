2d402d9bd2ed497fa5b63fbff5a6e3f4b2e2c8db
Seb Dalgarno <seb@poissonconsulting.ca>
2018-12-13 22:59:49 -0800

    move exported utils to tools script

--- R/utils.R
+++ R/utils.R
@@ -1,40 +1,3 @@
-#' Search valid GnisNames.
-#'
-#' @param pattern A character string containing a regular expression (or character string for fixed = TRUE) to be matched.
-#' @param ignore_case A flag indicating whether to ignore case.
-#' @param ... Arguments passed to grepl.
-#' @return A character vector.
-#' @examples
-#' fwa_search_gnis("kaslo")
-#' @export
-fwa_search_gnis <- function(pattern, ignore_case = TRUE, ...){
-  fwa_lookup_stream_gnis$GnisName[grepl(pattern, fwa_lookup_stream_gnis$GnisName, ignore.case = ignore_case, ...)]
-}
-
-#' Search valid watershed groups.
-#'
-#' @param pattern A character string containing a regular expression (or character string for fixed = TRUE) to be matched.
-#' @param ignore_case A flag indicating whether to ignore case.
-#' @param coast A flag indicating whether to only search watershed groups in the coastline layer.
-#' @param code A flag indicating whether to search by watershed group code (not name).
-#' @param ... Arguments passed to grepl.
-#' @return A character vector.
-#' @examples
-#' fwa_search_wsgroup("graham")
-#' @export
-fwa_search_wsgroup <- function(pattern, ignore_case = TRUE, coast = FALSE, code = FALSE, ...){
-  x <- fwa_lookup_wsgroup
-  if(coast){
-    x <- fwa_lookup_coast_wsgroup
-  }
-  y <- x$WatershedGroupName
-  if(code){
-    y <- x$WatershedGroupCode
-  }
-  y[grepl(pattern, y, ignore.case = ignore_case, ...)]
-}
-
-
 stream_to_blk <- function(x){
   unlist(lapply(x, function(x){
     if(is_blk_stream(x)){
@@ -81,8 +44,6 @@ tribs_wshed <- function(x){
     fwa_lookup_watershed$WatershedCode[grepl(x, fwa_lookup_watershed$WatershedCode, fixed = TRUE)]
   }))
 }
-
-
 
 line_sample <- function(data, distance){
   # data <- try(st_cast(data, "LINESTRING", silent = TRUE))
