92f79f671a82638841517ebd5f1710a5736b9b3d
Seb Dalgarno <seb@poissonconsulting.ca>
2019-04-25 23:34:01 -0700

    update lookup tables to stream-network and wskey

--- data-raw/internal-lookups.R
+++ data-raw/internal-lookups.R
@@ -3,9 +3,10 @@ library(dplyr)
 library(fwabc)
 
 lookup_stream_gnis <- fwa_lookup_stream[!is.na(fwa_lookup_stream$GNIS_NAME),]
+lookup_wshed_gnis <- fwa_lookup_watershed[!is.na(fwa_lookup_watershed$GNIS_NAME),]
 lookup_coast <- merge(fwa_lookup_coastline, fwa_lookup_watershedgroup, by = "WATERSHED_GROUP_CODE")
 lookup_coast_wsgroup <- lookup_coast %>%
   select(WATERSHED_GROUP_CODE, WATERSHED_GROUP_NAME) %>%
   distinct()
 
-use_data(lookup_stream_gnis, lookup_coast_wsgroup, internal = TRUE, overwrite = TRUE)
+use_data(lookup_stream_gnis, lookup_wshed_gnis, lookup_coast_wsgroup, internal = TRUE, overwrite = TRUE)
--- data-raw/lookups.R
+++ data-raw/lookups.R
@@ -3,17 +3,20 @@ library(sf)
 library(purrr)
 library(magrittr)
 library(checkr)
+library(tidyr)
 
 dsn_bc <- "~/Poisson/Data/spatial/fwa/gdb/FWA_BC.gdb/"
 dsn_wsl <- "~/Poisson/Data/spatial/fwa/gdb/FWA_WATERSHED_BOUNDARIES_SP.gdb/"
 dsn_wsp <- "~/Poisson/Data/spatial/fwa/gdb/FWA_WATERSHEDS_POLY.gdb/"
 dsn_lb <- "~/Poisson/Data/spatial/fwa/gdb/FWA_LINEAR_BOUNDARIES_SP.gdb/"
+dsn_st <- "~/Poisson/Data/spatial/fwa/gdb/FWA_STREAM_NETWORKS_SP.gdb/"
 
 ###### ------ layers and metadata
 fwa_metadata <- list(FWA_BC = st_layers(dsn_bc),
-                     FWA_WATERSHED_BOUNDARIES_SP = st_layers(dsn_wsl),
-                     FWA_WATERSHEDS_POLY = st_layers(dsn_wsp),
-                     FWA_LINEAR_BOUNDARIES_SP = st_layers(dsn_lb))
+                     FWA_STREAM_NETWORKS_SP = st_layers(dsn_st),
+                     # FWA_WATERSHED_BOUNDARIES_SP = st_layers(dsn_wsl),
+                     # FWA_LINEAR_BOUNDARIES_SP = st_layers(dsn_lb),
+                     FWA_WATERSHEDS_POLY = st_layers(dsn_wsp))
 
 fwa_layers <- lapply(fwa_metadata, function(x) x$name[!is.na(x$geomtype)])
 fwa_layers <- map_dfr(names(fwa_metadata), function(x){
@@ -25,65 +28,65 @@ usethis::use_data(fwa_metadata, overwrite = TRUE)
 usethis::use_data(fwa_layers, overwrite = TRUE)
 
 ###### ------ streams
-route <- st_read(dsn_bc, layer = "FWA_ROUTES_SP", query = "select BLUE_LINE_KEY, FWA_WATERSHED_CODE, WATERSHED_KEY from FWA_ROUTES_SP")
+layers <- st_layers(dsn_st)$name[1:246]
 
-route <- route %>%
-  st_set_geometry(NULL) %>%
-  select(BLUE_LINE_KEY, FWA_WATERSHED_CODE)
-route$FWA_WATERSHED_CODE %<>% as.character()
+stream <- map_dfr(layers, ~ st_read(dsn = dsn_st, layer = .,
+                                    query = paste("select FWA_WATERSHED_CODE,
+                                    WATERSHED_KEY, GNIS_NAME, WATERSHED_GROUP_CODE from", .)) %>%
+                                  st_set_geometry(NULL)) %>%
+  distinct()
 
-check_key(route, "BLUE_LINE_KEY")
+stream$FWA_WATERSHED_CODE %<>% as.character()
+stream$GNIS_NAME %<>% as.character()
 
-###### ------ gnis
-gnis <- st_read(dsn_bc, layer = "FWA_NAMED_WATERSHEDS_POLY") %>%
-  select(GNIS_NAME, BLUE_LINE_KEY)
-
-gnis <- gnis %>%
-  st_set_geometry(NULL)
-
-gnis$GNIS_NAME %<>% as.character()
-
-# remove Colley Creek BlueLineKey that isn't present in the route table
-gnis %<>% filter(!(BLUE_LINE_KEY %in% anti_join(gnis, route, "BLUE_LINE_KEY")$BLUE_LINE_KEY))
-check_join(gnis, route, "BLUE_LINE_KEY")
-
-fwa_lookup_stream <- route %>%
-  left_join(gnis, "BLUE_LINE_KEY")
-
-# note there are ~ 10 cases where the same BlueLineKey has multiple GnisNames!! BLUE_LINE_KEY is not the primary key
-check_key(fwa_lookup_stream, c("BLUE_LINE_KEY", "GNIS_NAME"))
+fwa_lookup_stream <- stream
 usethis::use_data(fwa_lookup_stream, overwrite = TRUE)
 
+###### ------ coastline
+coastline <- st_read(dsn_bc,
+                     layer = "FWA_COASTLINES_SP",
+                     query = "select WATERSHED_KEY, FWA_WATERSHED_CODE,
+                     WATERSHED_GROUP_CODE from FWA_COASTLINES_SP") %>%
+  st_set_geometry(NULL) %>%
+  distinct()
+
+coastline$WATERSHED_GROUP_CODE %<>% as.character()
+
+fwa_lookup_coastline <- coastline
+usethis::use_data(fwa_lookup_coastline, overwrite = TRUE)
+
 ###### ------ watershed groups
-wsgroup <- st_read(dsn_bc, layer = "FWA_WATERSHED_GROUPS_POLY")
+wsgroup <- st_read(dsn_bc,
+                   layer = "FWA_WATERSHED_GROUPS_POLY",
+                   query = "select WATERSHED_GROUP_CODE, WATERSHED_GROUP_NAME
+                   from FWA_WATERSHED_GROUPS_POLY")
 
 wsgroup <- wsgroup %>%
-  st_set_geometry(NULL) %>%
-  select(WATERSHED_GROUP_CODE, WATERSHED_GROUP_NAME)
+  st_set_geometry(NULL)
+
 wsgroup$WATERSHED_GROUP_CODE %<>% as.character()
 wsgroup$WATERSHED_GROUP_NAME %<>% as.character()
 
 fwa_lookup_watershedgroup <- wsgroup
 usethis::use_data(fwa_lookup_watershedgroup, overwrite = TRUE)
 
-###### ------ coastline
-coastline <- st_read(dsn_bc, layer = "FWA_COASTLINES_SP",
-                     query = "select BLUE_LINE_KEY, FWA_WATERSHED_CODE, WATERSHED_GROUP_CODE from FWA_COASTLINES_SP") %>%
-  st_set_geometry(NULL) %>%
-  distinct()
-
-coastline$WATERSHED_GROUP_CODE %<>% as.character()
-
-check_join(coastline, wsgroup, "WATERSHED_GROUP_CODE")
-fwa_lookup_coastline <- coastline
-usethis::use_data(fwa_lookup_coastline, overwrite = TRUE)
-
-###### ------ watershed polygons
+###### ------ watershed
 layers <- st_layers(dsn_wsp)$name
 
-fwa_lookup_watershed <- map_dfr(layers, ~ st_read(dsn = dsn_wsp, layer = ., query = paste("select FWA_WATERSHED_CODE, WATERSHED_GROUP_CODE from", .)) %>%
+fwa_lookup_watershed <- map_dfr(layers, ~ st_read(dsn = dsn_wsp,
+                                                  layer = .,
+                                                  query = paste("select WATERSHED_KEY,
+                                                  FWA_WATERSHED_CODE, WATERSHED_GROUP_CODE from", .)) %>%
                                           st_set_geometry(NULL)) %>%
   distinct()
+
+gnis <- stream %>%
+  select(WATERSHED_KEY, GNIS_NAME) %>%
+  distinct() %>%
+  filter(!is.na(GNIS_NAME))
+
+fwa_lookup_watershed <- fwa_lookup_watershed %>%
+  left_join(gnis, "WATERSHED_KEY")
 
 usethis::use_data(fwa_lookup_watershed, overwrite = TRUE)
 
