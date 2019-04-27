486f25e71c2fc1f015814454b2c1d121ffee29cc
Seb Dalgarno <seb@poissonconsulting.ca>
2019-04-27 15:42:08 -0700

    change fun names to match layer names. add check for watershed_groups

--- R/read.R
+++ R/read.R
@@ -1,15 +1,13 @@
 #' Read layer from FWA database.
 #'
-#' @param x A vector of valid WATERSHED_KEY or WATERSHED_GROUP_CODE.
+#' @param x A vector of valid WATERSHED_KEY or WATERSHED_GROUP_CODE. If NULL, entire dataset is read.
 #' @param layer A character string of valid layer name (see fwa_lookup_layers for reference).
 #' @param ask A flag indicating whether to ask before reading entire dataset.
 #' @return A sf object.
 #' @examples
 #' streams <- fwa_read_layer(c(360709847, 360843586), layer = "stream-network")
 #' @export
-fwa_read <- function(x = NULL, layer = "stream-network", ask = TRUE) {
-
-  check_layer(layer)
+fwa_read <- function(x = NULL, ask = TRUE, layer = "stream-network") {
 
   if(is.null(x)){
     if(!ask){
@@ -17,57 +15,62 @@ fwa_read <- function(x = NULL, layer = "stream-network", ask = TRUE) {
     }
     if(yesno::yesno("This is a very large dataset. Do you want to download the entire ",
                     layer, " layer?")){
-      all_data(layer)
+      return(all_data(layer))
     }
+    return()
   }
+
   check_wskey_wsgcode(x)
   x <- wsgcode_to_wskey(x)
 
-  some_data(layer, x)
+  bcdata::bcdc_query_geodata(paste0("freshwater-atlas-", layer)) %>%
+    bcdata::filter(WATERSHED_KEY %in% c(360709847, 360843586)) %>%
+    bcdata::collect()
 }
 
 #' Read features from stream-network layer.
 #'
-#' @param x A vector of valid WATERSHED_KEY or WATERSHED_GROUP_CODE.
-#' @param ask A flag indicating whether to ask before reading entire dataset.
+#' @inheritParams fwa_read
 #' @return A sf object.
 #' @examples
 #' all <- fwa_read_stream()
 #' streams <- fwa_read_stream(c(360709847, 360843586))
 #' @export
 fwa_read_stream <- function(x = NULL, ask = TRUE) {
-  fwa_read(x = x, ask = ask, layer = "stream-network")
+ fwa_read(x = x, ask = ask, layer = "stream-network")
 }
 
 #' Read watershed from FWA_WATERSHEDS_POLY geodatabase.
 #'
-#' @inheritParams fwa_read_stream
+#' @inheritParams fwa_read
 #' @return A sf object.
 #' @examples
 #' watershed <- fwa_read_watershed(c(360709847, 360843586))
 #' @export
-fwa_read_watershed <- function(x = NULL, ask = TRUE) {
+fwa_read_watersheds <- function(x = NULL, ask = TRUE) {
   fwa_read(x = x, ask = ask, layer = "watersheds")
 }
 
 #' Read coastline from FWA_COASTLINES_SP layer in FWA_BC geodatabase.
 #'
-#' @inheritParams fwa_read_stream
+#' @inheritParams fwa_read
 #' @return A sf object.
 #' @examples
 #' coastline <- fwa_read_coastline("GRAI")
 #' @export
-fwa_read_coastline <- function(x = NULL, ask = FALSE) {
-  fwa_read(x = x, ask = ask, layer = "coastline")
+fwa_read_coastlines <- function(x = NULL, ask = FALSE) {
+  fwa_read(x = x, ask = ask, layer = "coastlines")
 }
 
 #' Read watershed group from FWA_WATERSHED_GROUPS layer in FWA_BC geodatabase
 #'
-#' @inheritParams fwa_read_stream
+#' @param x A vector of valid WATERSHED_GROUP_CODE. If NULL, entire dataset is read.
+#' @inheritParams fwa_read
 #' @return A sf object.
 #' @examples
 #' graham <- fwa_read_watershed_group("GRAI")
 #' @export
-fwa_read_watershed_group <- function(x = NULL, ask = FALSE) {
+fwa_read_watershed_groups <- function(x = NULL, ask = FALSE) {
+  check_wsgcode(x)
   fwa_read(x = x, ask = ask, layer = "watershed-groups")
 }
