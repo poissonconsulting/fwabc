0d33d2040ec635f8ab5084d053adb1f030dc6905
Seb Dalgarno <seb@poissonconsulting.ca>
2019-04-27 15:46:14 -0700

    watershed_groups special case

--- R/read.R
+++ R/read.R
@@ -1,4 +1,4 @@
-#' Read layer from FWA database.
+#' Read features from FWA database layer.
 #'
 #' @param x A vector of valid WATERSHED_KEY or WATERSHED_GROUP_CODE. If NULL, entire dataset is read.
 #' @param layer A character string of valid layer name (see fwa_lookup_layers for reference).
@@ -28,7 +28,7 @@ fwa_read <- function(x = NULL, ask = TRUE, layer = "stream-network") {
     bcdata::collect()
 }
 
-#' Read features from stream-network layer.
+#' Read from stream-network layer.
 #'
 #' @inheritParams fwa_read
 #' @return A sf object.
@@ -40,7 +40,7 @@ fwa_read_stream <- function(x = NULL, ask = TRUE) {
  fwa_read(x = x, ask = ask, layer = "stream-network")
 }
 
-#' Read watershed from FWA_WATERSHEDS_POLY geodatabase.
+#' Read from watersheds layer.
 #'
 #' @inheritParams fwa_read
 #' @return A sf object.
@@ -51,7 +51,7 @@ fwa_read_watersheds <- function(x = NULL, ask = TRUE) {
   fwa_read(x = x, ask = ask, layer = "watersheds")
 }
 
-#' Read coastline from FWA_COASTLINES_SP layer in FWA_BC geodatabase.
+#' Read from coastlines layer.
 #'
 #' @inheritParams fwa_read
 #' @return A sf object.
@@ -62,7 +62,7 @@ fwa_read_coastlines <- function(x = NULL, ask = FALSE) {
   fwa_read(x = x, ask = ask, layer = "coastlines")
 }
 
-#' Read watershed group from FWA_WATERSHED_GROUPS layer in FWA_BC geodatabase
+#' Read from watershed-groups layer.
 #'
 #' @param x A vector of valid WATERSHED_GROUP_CODE. If NULL, entire dataset is read.
 #' @inheritParams fwa_read
@@ -71,6 +71,20 @@ fwa_read_coastlines <- function(x = NULL, ask = FALSE) {
 #' graham <- fwa_read_watershed_group("GRAI")
 #' @export
 fwa_read_watershed_groups <- function(x = NULL, ask = FALSE) {
+  if(is.null(x)){
+    if(!ask){
+      return(all_data(layer))
+    }
+    if(yesno::yesno("This is a very large dataset. Do you want to download the entire ",
+                    layer, " layer?")){
+      return(all_data(layer))
+    }
+    return()
+  }
+
   check_wsgcode(x)
-  fwa_read(x = x, ask = ask, layer = "watershed-groups")
+
+  bcdata::bcdc_query_geodata(paste0("freshwater-atlas-watershed-groups")) %>%
+    bcdata::filter(WATERSHED_KEY %in% "PORI") %>%
+    bcdata::collect()
 }
