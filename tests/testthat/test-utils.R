760ac4403a04ba7df31171d06570d96a6a01f0fb
Seb Dalgarno <seb@poissonconsulting.ca>
2019-05-01 17:31:24 -0700

    update tests for utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,52 +3,54 @@ context("utils")
 test_that("utils work", {
 
   ### translators
-  x <- stream_to_blk(c("Kaslo River", 356567321L))
-  expect_length(x, 2L)
-  expect_is(x, "character")
-  expect_true(all(x == 356567321L))
+  x <- is_wskey(c("Kaslo River", 356567321L))
+  expect_identical(x, c(FALSE, TRUE))
+  expect_false(is_wskey(356567321L, layer = "coastlines"))
 
-  x <- stream_to_wscode(c("Kaslo River", "300-625474-095953-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000"))
-  expect_length(x, 2L)
-  expect_is(x, "character")
-  expect_true(all(x == "300-625474-095953-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000-000000"))
+  y <- lookup_wskey$FWA_WATERSHED_CODE[1]
+  x <- is_wscode(c(y, 356567321L))
+  expect_identical(x, c(TRUE, FALSE))
+  expect_false(is_wskey(y, layer = "coastlines"))
 
-  x <- wsgname_to_wsgcode(c("Porcher Island", "PORI", 356567321L))
-  expect_length(x, 3L)
-  expect_is(x, "character")
-  expect_identical(x,  c("PORI", "PORI", "356567321"))
+  x <- is_wsgcode(c("GRAI", 356567321L))
+  expect_identical(x, c(TRUE, FALSE))
+  expect_false(is_wsgcode("UPET", layer = "coastlines"))
+
+  x <- is_wsgname(c("Graham Island", 356567321L))
+  expect_identical(x, c(TRUE, FALSE))
+  expect_false(is_wsgname("Upper Shuswap", layer = "coastlines"))
+
+  x <- is_gnis(c("Sangan River", 356567321L))
+  expect_identical(x, c(TRUE, FALSE))
+  expect_false(is_wskey("Porcher Creek", layer = "coastlines"))
 
   ### tributaries
-  x <- tribs_stream(stream_to_wscode("Chown Brook"))
-  expect_is(x, "integer")
-  expect_true(all(is_blk_stream(x)))
+  x <- tribs(lookup_wskey$FWA_WATERSHED_CODE[1], n = 1L)
+  expect_true(all(is_wskey(x)))
   expect_length(x, 8L)
 
-  y <- tribs_stream(stream_to_wscode("Chown Brook"), n = 1L)
-  expect_is(x, "integer")
-  expect_true(all(is_blk_stream(x)))
-  expect_length(x, 3L)
+  x <- tribs(lookup_wskey$FWA_WATERSHED_CODE[1], n = 2L)
+  expect_true(all(is_wskey(x)))
+  expect_length(x, 10L)
 
-  expect_true(all(y %in% x))
+  ### converters
+  y <- c(lookup_wskey$WATERSHED_KEY[1], lookup_wskey$FWA_WATERSHED_CODE[2])
+  x <- wskey_to_wscode(y)
+  expect_length(x, 2L)
+  expect_identical(x[2], lookup_wskey$FWA_WATERSHED_CODE[1])
+  expect_true(all(is_wscode(x)))
 
-  x <- tribs_wshed(stream_to_wscode("Chown Brook"))
-  expect_is(x, "character")
-  expect_true(all(is_ws_code_stream(x)))
-  expect_length(x, 8L)
+  wsg <- unique(lookup_wskey$WATERSHED_GROUP_CODE)[2]
+  y <- c(lookup_wskey$WATERSHED_KEY[1], wsg)
+  x <- wsgcode_to_wskey(y)
+  z <- wsgcode_to_wskey(wsg)
+  expect_true(length(x) - length(z) == 1)
+  expect_true(all(is_wskey(x)))
+  expect_length(x, 1464L)
+  expect_identical(setdiff(x, z), lookup_wskey$WATERSHED_KEY[1])
 
-  y <- tribs_wshed(stream_to_wscode("Chown Brook"), n = 1L)
-  expect_is(x, "character")
-  expect_true(all(is_ws_code_stream(x)))
-  expect_length(x, 3L)
+  wsk <- x[1:2]
+  wsg2 <- wskey_to_wsgcode(x[1:2])
+  expect_identical(wsg2, c("PORI", "UPET"))
 
-  expect_true(all(y %in% x))
-
-  ### search funs
-  expect_identical(fwa_search_gnis("kaslo"), "Kaslo River")
-  expect_identical(character(0), fwa_search_gnis("kaslo", ignore_case = FALSE))
-
-  expect_identical(fwa_search_wsgroup("porcher"), "Porcher Island")
-  expect_identical(character(0), fwa_search_wsgroup("porcher", code = TRUE))
-  expect_identical(character(0), fwa_search_wsgroup("kootenay", coast = TRUE))
-  expect_length(fwa_search_wsgroup("kootenay"), 2L)
 })
