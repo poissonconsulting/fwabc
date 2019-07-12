338147379271642a168339db6b36527596c20516
Seb Dalgarno <seb@poissonconsulting.ca>
2019-07-12 15:38:02 -0700

    remove stream_order to generalise across layers

--- R/read-layer.R
+++ R/read-layer.R
@@ -1,67 +1,51 @@
 read_layer <- function(what, layer, x,
-                       named_only, tributaries,
-                       min_stream_order, crs){
+                       named_only, tributaries, crs){
   x <- switch(what,
-              "GNIS_NAME" = read_layer_gnis(layer, x,
-                                            tributaries, min_stream_order, crs),
-              "WATERSHED_GROUP_CODE" = read_layer_wsgcode(layer, x, named_only,
-                                                          min_stream_order, crs),
-              "WATERSHED_KEY" = read_layer_wskey(layer, x, named_only,
-                                                 tributaries, min_stream_order, crs),
-              "NULL" = read_layer_null(layer, x, named_only,
-                                       min_stream_order, crs),
+              "GNIS_NAME" = read_layer_gnis(layer, x, tributaries, crs),
+              "WATERSHED_GROUP_CODE" = read_layer_wsgcode(layer, x, named_only, crs),
+              "WATERSHED_KEY" = read_layer_wskey(layer, x, named_only, tributaries, crs),
+              "NULL" = read_layer_null(layer, x, named_only, crs),
               NULL)
 }
 
-read_layer_gnis <- function(layer, x, tributaries,
-                            min_stream_order, crs){
+read_layer_gnis <- function(layer, x, named_only, tributaries, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs)
-  if(tributaries){
+  if(named_only && tributaries){
     code <- gnis_to_wscode(x)
     return(y %>% bcdata::filter(CQL(glue("FWA_WATERSHED_CODE LIKE '{code}%'"))) %>%
-             bcdata::filter(STREAM_ORDER >= min_stream_order))
+             bcdata::filter(!is.na(GNIS_NAME)))
   }
-  y %>% bcdata::filter(GNIS_NAME %in% x,
-                       STREAM_ORDER >= min_stream_order)
+  y %>% bcdata::filter(GNIS_NAME %in% x)
 }
 
-read_layer_wskey <- function(layer, x, named_only, tributaries,
-                            min_stream_order, crs){
+read_layer_wskey <- function(layer, x, named_only, tributaries, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs)
   if(named_only && tributaries){
     code <- wskey_to_wscode(x)
     return(y %>% bcdata::filter(CQL(glue("FWA_WATERSHED_CODE LIKE '{code}%'"))) %>%
-             bcdata::filter(STREAM_ORDER >= min_stream_order,
-                            !is.na(GNIS_NAME)))
+             bcdata::filter(!is.na(GNIS_NAME)))
   }
   if(named_only && !tributaries){
     return(y %>% bcdata::filter(WATERSHED_KEY %in% x,
-                                STREAM_ORDER >= min_stream_order,
                                 !is.na(GNIS_NAME)))
 
   }
-  y %>% bcdata::filter(WATERSHED_KEY %in% x,
-                       STREAM_ORDER >= min_stream_order)
+  y %>% bcdata::filter(WATERSHED_KEY %in% x)
 }
 
-read_layer_wsgcode <- function(layer, x, named_only,
-                            min_stream_order, crs){
+read_layer_wsgcode <- function(layer, x, named_only, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs)
   if(named_only){
     return(y %>% bcdata::filter(WATERSHED_GROUP_CODE %in% x,
-                                STREAM_ORDER >= min_stream_order,
                                 !is.na(GNIS_NAME)))
   }
-  y %>% bcdata::filter(WATERSHED_GROUP_CODE %in% x,
-                       STREAM_ORDER >= min_stream_order)
+  y %>% bcdata::filter(WATERSHED_GROUP_CODE %in% x)
 }
 
-read_layer_null <- function(layer, x, named_only,
-                            min_stream_order, crs){
+read_layer_null <- function(layer, x, named_only, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs)
   if(named_only){
-    return(y %>% bcdata::filter(STREAM_ORDER >= min_stream_order,
-                                !is.na(GNIS_NAME)))
+    return(y %>% bcdata::filter(!is.na(GNIS_NAME)))
   }
-  y %>% bcdata::filter(STREAM_ORDER >= min_stream_order)
+  y
 }
