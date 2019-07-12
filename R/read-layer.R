15e26c31b30b578db8c4304bd3ca78480e3c0862
Seb Dalgarno <seb@poissonconsulting.ca>
2019-07-12 16:44:32 -0700

    cant have functions inside CQL

--- R/read-layer.R
+++ R/read-layer.R
@@ -1,7 +1,7 @@
 read_layer <- function(what, layer, x,
                        named_only, tributaries, crs){
   x <- switch(what,
-              "GNIS_NAME" = read_layer_gnis(layer, x, tributaries, crs),
+              "GNIS_NAME" = read_layer_gnis(layer, x, named_only, tributaries, crs),
               "WATERSHED_GROUP_CODE" = read_layer_wsgcode(layer, x, named_only, crs),
               "WATERSHED_KEY" = read_layer_wskey(layer, x, named_only, tributaries, crs),
               "NULL" = read_layer_null(layer, x, named_only, crs),
@@ -11,9 +11,11 @@ read_layer <- function(what, layer, x,
 read_layer_gnis <- function(layer, x, named_only, tributaries, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs)
   if(named_only && tributaries){
-    code <- gnis_to_wscode(x)
-    return(y %>% bcdata::filter(CQL(glue("FWA_WATERSHED_CODE LIKE '{code}%'"))) %>%
-             bcdata::filter(!is.na(GNIS_NAME)))
+    cql <- paste0("FWA_WATERSHED_CODE LIKE '", gnis_to_wscode(x), "-%'")
+    return(
+      y %>% bcdata::filter(CQL(cql)) %>%
+             bcdata::filter(!is.na(GNIS_NAME))
+      )
   }
   y %>% bcdata::filter(GNIS_NAME %in% x)
 }
@@ -21,8 +23,8 @@ read_layer_gnis <- function(layer, x, named_only, tributaries, crs){
 read_layer_wskey <- function(layer, x, named_only, tributaries, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs)
   if(named_only && tributaries){
-    code <- wskey_to_wscode(x)
-    return(y %>% bcdata::filter(CQL(glue("FWA_WATERSHED_CODE LIKE '{code}%'"))) %>%
+    cql <- paste0("FWA_WATERSHED_CODE LIKE '", wskey_to_wscode(x), "-%'")
+    return(y %>% bcdata::filter(CQL(cql)) %>%
              bcdata::filter(!is.na(GNIS_NAME)))
   }
   if(named_only && !tributaries){
