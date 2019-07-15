1f5497c984a18530922ace8b01a31334c6d2070f
Seb Dalgarno <seb@poissonconsulting.ca>
2019-07-15 15:15:33 -0700

    better system for checking x and x within layers

--- R/checks.R
+++ R/checks.R
@@ -1,37 +1,62 @@
-check_gkcn <- function(x){
-  if(all(is_gnis(x)) || all(is_wsgcode(x)) ||
-     all(is_wsgname(x)) || all(is_wskey(x)))
-    return(x)
-  err("x must be a vector of valid GNIS_NAME, WATERSHED_KEY,
+check_x <- function(x, types){
+  if(types == "gkcn"){
+    if(!(all(is_gnis(x)) || all(is_wsgcode(x)) ||
+      all(is_wsgname(x)) || all(is_wskey(x))))
+      err("x must be a vector of valid GNIS_NAME, WATERSHED_KEY,
       WATERSHED_GROUP_CODE, or WATERSHED_GROUP_NAME")
-}
+  }
 
-check_gkcn_layer <- function(x, layer){
-  what <- what_is_it(x[1])
-  x <- switch(what,
-         "GNIS_NAME" = x[!is_gnis(x, layer)],
-         "WATERSHED_GROUP_CODE" = x[!is_wsgcode(x, layer)],
-         "WATERSHED_GROUP_NAME" = x[!is_wsgcode(x, layer)],
-         "WATERSHED_KEY" = x[!is_wskey(x, layer)])
-  if(length(x) > 0)
-    err(co(x, some = paste0("%c ", what, "%r not available for layer '", layer, "'"),
-           one = paste0("%c ", what, " is not available for layer '", layer, "'"),  conjunction = "and"))
+  if(types == "kcn"){
+    if(!(all(is_wsgcode(x)) ||
+         all(is_wsgname(x)) || all(is_wskey(x))))
+      err("x must be a vector of valid WATERSHED_KEY,
+      WATERSHED_GROUP_CODE, or WATERSHED_GROUP_NAME")
+  }
+
+  if(types == "cn"){
+    if(!(all(is_wsgname(x)) || all(is_wskey(x))))
+      err("x must be a vector of WATERSHED_GROUP_CODE or WATERSHED_GROUP_NAME")
+  }
+
+  if(types == "gk"){
+    if(!(all(is_gnis(x)) || all(is_wsgcode(x))))
+      err("x must be a vector of valid GNIS_NAME or WATERSHED_KEY")
+  }
   x
 }
 
-check_kcn <- function(x){
-  if(all(is_wskey(x)) ||
-     all(is_wsgcode(x)) || all(is_wsgname(x)))
-    return(x)
-  err("x must be a vector of valid WATERSHED_KEY,
-      WATERSHED_GROUP_CODE, or WATERSHED_GROUP_NAME")
-}
+check_x_layer <- function(x, types, layer){
+  what <- what_is_it(x[1])
+  if(types == "gkcn"){
+    y <- switch(what,
+                "GNIS_NAME" = x[!is_gnis(x, layer)],
+                "WATERSHED_GROUP_CODE" = x[!is_wsgcode(x, layer)],
+                "WATERSHED_GROUP_NAME" = x[!is_wsgname(x, layer)],
+                "WATERSHED_KEY" = x[!is_wskey(x, layer)])
+  }
 
-check_cn <- function(x){
-  if(all(is_wsgcode(x)) || all(is_wsgname(x)))
-    return(x)
-  err("x must be a vector of valid
-      WATERSHED_GROUP_CODE, or WATERSHED_GROUP_NAME")
+  if(types == "kcn"){
+    y <- switch(what,
+                "WATERSHED_GROUP_CODE" = x[!is_wsgcode(x, layer)],
+                "WATERSHED_GROUP_NAME" = x[!is_wsgname(x, layer)],
+                "WATERSHED_KEY" = x[!is_wskey(x, layer)])
+  }
+
+  if(types == "cn"){
+    y <- switch(what,
+                "WATERSHED_GROUP_CODE" = x[!is_wsgcode(x, layer)],
+                "WATERSHED_GROUP_NAME" = x[!is_wsgname(x, layer)])
+  }
+
+  if(types == "gk"){
+    y <- switch(what,
+                "GNIS_NAME" = x[!is_gnis(x, layer)],
+                "WATERSHED_KEY" = x[!is_wskey(x, layer)])
+  }
+  if(length(y) > 0)
+    err(co(x, some = paste0("%c ", what, "%r not available for layer '", layer, "'"),
+           one = paste0("%c ", what, " is not available for layer '", layer, "'"),  conjunction = "and"))
+  x
 }
 
 check_tributaries <- function(x){
--- R/watersheds.R
+++ R/watersheds.R
@@ -1,35 +1,32 @@
-#' Read features from 'freshwater-atlas-stream-network' dataset.
+#' Read features from 'freshwater-atlas-watersheds' dataset.
 #'
-#' @param x A vector of valid GNIS_NAME, WATERSHED_KEY, WATERSHED_GROUP_NAME or WATERSHED_GROUP_CODE.
+#' @param x A vector of valid WATERSHED_KEY, WATERSHED_GROUP_NAME or WATERSHED_GROUP_CODE.
 #' If NULL, entire dataset is read.
-#' @param named_only A flag indicating whether to only include features with GNIS_NAME.
 #' @param tributaries A flag indicating whether to read all tributaries in addition
 #'  (only applicable if x is GNIS_NAME or WATERSHED_KEY).
-#' @param min_stream_order An integer indicating minimum STREAM_ORDER to read.
+#' @param min_watershed_order An integer indicating minimum WATERSHED_ORDER to read.
 #' @param crs The epsg code for the coordinate reference system. Defaults to `3005`
 #'        (B.C. Albers). See https://epsgi.io.
 #' @param collect A flag indicating whether to collect result.
 #' @return A sf object if collect = TRUE, otherwise a object of class bcdc_promise.
 #' @examples
 #'\dontrun{
-#' fwa_read_stream_network("Fraser River")
+#' fwa_read_watersheds("Upper Fraser River")
 #' }
 #' @export
-fwa_read_stream_network <- function(x = NULL,
-                                    named_only = FALSE,
+fwa_read_watersheds <- function(x = NULL,
                                     tributaries = FALSE,
-                                    min_stream_order = 1L,
+                                    min_watershed_order = 1L,
                                     crs = 3005,
                                     collect = TRUE) {
 
   if(!is.null(x)){
-    check_x(x, "gkcn")
-    check_x_layer(x, "gkcn", "stream-network")
+    check_x(x, "kcn")
+    check_x_layer(x, "kcn", "watersheds")
   }
 
-  check_flag(named_only)
   check_flag(tributaries)
-  check_integer(min_stream_order)
+  check_integer(min_watershed_order)
   check_numeric(crs)
 
   if(is_wsgname(x[1])){
@@ -38,13 +35,13 @@ fwa_read_stream_network <- function(x = NULL,
 
   what <- what_is_it(x[1])
   x <- read_layer(what = what,
-                  layer = "stream-network",
+                  layer = "watersheds",
                   x = x,
-                  named_only = named_only,
+                  named_only = FALSE,
                   tributaries = tributaries,
-                  multiple_gnis = FALSE,
+                  multiple_gnis = TRUE,
                   crs = crs) %>%
-    bcdata::filter(STREAM_ORDER >= min_stream_order)
+    bcdata::filter(WATERSHED_ORDER >= min_watershed_order)
 
   if(collect)
     return(x %>% bcdata::collect())
