584a64e9cea5e37e6e36ad87cc569c305c70a25d
Seb Dalgarno <seb@poissonconsulting.ca>
2019-07-15 15:15:50 -0700

    organise args and if statements

--- R/read-layer.R
+++ R/read-layer.R
@@ -1,13 +1,28 @@
 read_layer <- function(what, layer, x, named_only,
                        tributaries, multiple_gnis, crs){
   x <- switch(what,
-              "GNIS_NAME" = read_layer_gnis(layer, x, named_only,
-                                            tributaries, multiple_gnis, crs),
-              "WATERSHED_GROUP_CODE" = read_layer_wsgcode(layer, x, named_only,
-                                                          multiple_gnis, crs),
-              "WATERSHED_KEY" = read_layer_wskey(layer, x, named_only,
-                                                 tributaries, multiple_gnis, crs),
-              "NULL" = read_layer_null(layer, x, named_only, multiple_gnis, crs),
+              "GNIS_NAME" = read_layer_gnis(layer = layer,
+                                            x = x,
+                                            named_only = named_only,
+                                            tributaries = tributaries,
+                                            multiple_gnis = multiple_gnis,
+                                            crs = crs),
+              "WATERSHED_GROUP_CODE" = read_layer_wsgcode(layer = layer,
+                                                          x = x,
+                                                          named_only = named_only,
+                                                          multiple_gnis = multiple_gnis,
+                                                          crs = crs),
+              "WATERSHED_KEY" = read_layer_wskey(layer = layer,
+                                                 x = x,
+                                                 named_only = named_only,
+                                                 tributaries = tributaries,
+                                                 multiple_gnis = multiple_gnis,
+                                                 crs = crs),
+              "NULL" = read_layer_null(layer = layer,
+                                       x = x,
+                                       named_only = named_only,
+                                       multiple_gnis = multiple_gnis,
+                                       crs = crs),
               NULL)
 }
 
@@ -24,13 +39,14 @@ read_layer_gnis <- function(layer, x, named_only,
     y <- bcdata::filter(GNIS_NAME == x)
   }
 
-  if(named_only && !multiple_gnis){
-    return(y %>% bcdata::filter(!is.na(GNIS_NAME)))
-  } else if(named_only && multiple_gnis){
-    return(y %>% bcdata::filter(!is.na(GNIS_NAME_1)))
-  } else {
-    return(y)
+  if(named_only){
+    if(multiple_gnis){
+      return(y %>% filter(!is.na(GNIS_NAME_1)))
+    } else {
+      return(y %>% filter(!is.na(GNIS_NAME)))
+    }
   }
+  y
 }
 
 read_layer_wskey <- function(layer, x, named_only,
@@ -44,33 +60,39 @@ read_layer_wskey <- function(layer, x, named_only,
     y <- bcdata::filter(WATERSHED_KEY == x)
   }
 
-  if(named_only && !multiple_gnis){
-    return(y %>% bcdata::filter(!is.na(GNIS_NAME)))
-  } else if(named_only && multiple_gnis){
-    return(y %>% bcdata::filter(!is.na(GNIS_NAME_1)))
-  } else {
-    return(y)
+  if(named_only){
+    if(multiple_gnis){
+      return(y %>% filter(!is.na(GNIS_NAME_1)))
+    } else {
+      return(y %>% filter(!is.na(GNIS_NAME)))
+    }
   }
+  y
 }
 
 read_layer_wsgcode <- function(layer, x, named_only,
                                multiple_gnis, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs) %>%
     bcdata::filter(WATERSHED_GROUP_CODE %in% x)
-  if(multiple_gnis){
-    return(y %>% filter(!is.na(GNIS_NAME_1)))
+  if(named_only){
+    if(multiple_gnis){
+      return(y %>% filter(!is.na(GNIS_NAME_1)))
+    } else {
+      return(y %>% filter(!is.na(GNIS_NAME)))
+    }
   }
-  y %>% filter(!is.na(GNIS_NAME))
+  y
 }
 
 read_layer_null <- function(layer, x, named_only,
                             multiple_gnis, crs){
   y <- bcdata::bcdc_query_geodata(lookup_record[[layer]], crs = crs)
-  if(named_only && !multiple_gnis){
-    return(y %>% bcdata::filter(!is.na(GNIS_NAME)))
-  } else if(named_only && multiple_gnis){
-    return(y %>% bcdata::filter(!is.na(GNIS_NAME_1)))
-  } else {
-    return(y)
+  if(named_only){
+    if(multiple_gnis){
+      return(y %>% filter(!is.na(GNIS_NAME_1)))
+    } else {
+      return(y %>% filter(!is.na(GNIS_NAME)))
+    }
   }
+  y
 }
