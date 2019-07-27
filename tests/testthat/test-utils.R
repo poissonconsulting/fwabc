bd6d79f5675abe2c4e2151a85b6d9281113a4165
Seb Dalgarno <seb@poissonconsulting.ca>
2019-07-26 22:38:50 -0700

    update util tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,6 +6,7 @@ test_that("utils work", {
   x <- is_wskey(c("Kaslo River", 356567321L))
   expect_identical(x, c(FALSE, TRUE))
   expect_false(is_wskey(356567321L, layer = "coastlines"))
+  expect_true(is_wskey(356567321L, layer = "stream-network"))
 
   y <- lookup_wskey$FWA_WATERSHED_CODE[1]
   x <- is_wscode(c(y, 356567321L))
@@ -15,47 +16,33 @@ test_that("utils work", {
   x <- is_wsgcode(c("GRAI", 356567321L))
   expect_identical(x, c(TRUE, FALSE))
   expect_false(is_wsgcode("UPET", layer = "coastlines"))
+  expect_true(is_wsgcode("UPET", layer = "watershed-groups"))
 
   x <- is_wsgname(c("Graham Island", 356567321L))
   expect_identical(x, c(TRUE, FALSE))
   expect_false(is_wsgname("Upper Shuswap", layer = "coastlines"))
+  expect_true(is_wsgname("Upper Shuswap", layer = "watershed-groups"))
 
   x <- is_gnis(c("Sangan River", 356567321L))
   expect_identical(x, c(TRUE, FALSE))
   expect_false(is_wskey("Porcher Creek", layer = "coastlines"))
-
-  ### tributaries
-  x <- tribs(lookup_wskey$FWA_WATERSHED_CODE[1], n = 1L)
-  expect_true(all(is_wskey(x)))
-  expect_length(x, 8L)
-
-  x <- tribs(lookup_wskey$FWA_WATERSHED_CODE[1], n = 2L)
-  expect_true(all(is_wskey(x)))
-  expect_length(x, 10L)
+  expect_false(is_gnis("Porcher Creek", layer = "glaciers"))
+  expect_true(is_gnis("Porcher Creek", layer = "stream-network"))
 
   ### converters
   y <- c(lookup_wskey$WATERSHED_KEY[1], lookup_wskey$FWA_WATERSHED_CODE[2])
-  x <- wskey_to_wscode(y)
-  expect_length(x, 2L)
-  expect_identical(x[2], lookup_wskey$FWA_WATERSHED_CODE[1])
-  expect_true(all(is_wscode(x)))
+  x <- wskey_to_wscode(y, layer = "stream-network")
+  expect_identical(x, "915-749008-900977")
+  expect_length(wskey_to_wscode(y, layer = "manmade-waterbodies"), 0L)
 
-  ### filters
-  wskey <- c(360709847, 360843586)
-  x <- filter_wskey(wskey, layer = "stream-network", crs = 3005)
-  expect_identical(nrow(x), 43L)
-  expect_is(x, "sf")
-  expect_equal(sf::st_crs(x)$epsg, 3005)
+  y <- c(lookup_gnis$GNIS_NAME[1], lookup_gnis$FWA_WATERSHED_CODE[2])
+  x <- gnis_to_wscode(y, layer = "stream-network")
+  expect_identical(x, "915-724877-268923")
+  expect_length(gnis_to_wscode(y, layer = "manmade-waterbodies"), 0L)
 
-  wsgcode <- c("VICT", "LKEL")
-  x <- filter_wsgcode(wsgcode, layer = "stream-network", crs = 4326)
-  expect_equal(sf::st_crs(x)$epsg, 4326L)
-  expect_identical(nrow(x), 4526L)
-  expect_is(x, "sf")
-
-  x <- filter_both(wskey = 360709847, wsgcode = "VICT",
-                   layer = "stream-network", crs = 4326)
-  expect_identical(nrow(x), 2161L)
-  expect_is(x, "sf")
+  y <- "Graham Island"
+  x <- wsgname_to_wsgcode(y)
+  expect_identical(x, "GRAI")
+  expect_identical(wsgcode_to_wsgname(x), y)
 
 })
